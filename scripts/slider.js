1c98d9383c3a3fa1504fe7236aca22a81333a250
SantinoVicentini <vicen.lan2017@gmail.com>
2023-06-27 01:38:55 -0300

    home design

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,259 +1,65 @@
-
-  const popButton80 = document.getElementById('popButton80');
-  const rockButton80 = document.getElementById('rockButton80');
-  const popButton90 = document.getElementById('popButton90');
-  const rockButton90 = document.getElementById('rockButton90');
-  const popButton00 = document.getElementById('popButton00');
-  const rockButton00 = document.getElementById('rockButton00');
-  const popButton10 = document.getElementById('popButton10');
-  const rockButton10 = document.getElementById('rockButton10');
-  
-  const popList80 = document.getElementById('80pop');
-  const billieJean = document.getElementById('BillieJean');
-  const whenDovesCry = document.getElementById('WhenDovesCry');
-  
-  const rockList80 = document.getElementById('80rock');
-  const livin = document.getElementById('BonJovi');
-  const guns = document.getElementById('SweetChild');
-  const under = document.getElementById('UnderPressure');
-  
-  const popList90 = document.getElementById('90pop');
-  const wannabe = document.getElementById('Wannabe');
-  const britney = document.getElementById('Britney');
-  
-  const rockList90 = document.getElementById('90rock');
-  const dosTierras = document.getElementById('2t');
-  const nirvana = document.getElementById('Nirvana');
-  const wonderwall = document.getElementById('Wonderwall');
-  
-  const popList00 = document.getElementById('00pop');
-  const crazy = document.getElementById('Crazy');
-  const badRomance = document.getElementById('BadRomance');
-  const girl = document.getElementById('Wagw');
-  
-  const rockList00 = document.getElementById('00rock');
-  const dios = document.getElementById('Dios');
-  const end = document.getElementById('End');
-  
-  const popList10 = document.getElementById('10pop');
-  const shake = document.getElementById('Sio');
-  const adele = document.getElementById('Adele');
-  const work = document.getElementById('Work');
-  
-  const rockList10 = document.getElementById('10rock');
-  const arctic = document.getElementById('Diwk');
-  const radioactive = document.getElementById('Radioactive');
-  
-  const iframeContainer = document.getElementById('iframeContainer');
-  const iframeContainer2 = document.getElementById('iframeContainer2');
-  const iframeContainer3 = document.getElementById('iframeContainer3');
-  const iframeContainer4 = document.getElementById('iframeContainer4');
-
-  const botones80 = document.getElementById('botones');
+var button = document.getElementById("update-button");
+var button2 = document.getElementById("update-button2");
+var button3 = document.getElementById("update-button3");
+var button4 = document.getElementById("update-button4");
+var button5 = document.getElementById("update-button5");
   
   
-  
-  
-  popButton80.addEventListener('click', function() {
-      popList80.style.display = 'flex';
-      rockList80.style.display = 'none';
-  });
-  
-  billieJean.addEventListener('click', function() {
-      iframeContainer.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5ChkMS8OtdzJeqyybCc9R5?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  whenDovesCry.addEventListener('click', function() {
-      iframeContainer.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/51H2y6YrNNXcy3dfc3qSbA?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  rockButton80.addEventListener('click', function() {
-      rockList80.style.display = 'flex';
-      popList80.style.display = 'none';
-  });
-  
-  livin.addEventListener('click', function() {
-      iframeContainer.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/37ZJ0p5Jm13JPevGcx4SkF?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  guns.addEventListener('click', function() {
-      iframeContainer.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7snQQk1zcKl8gZ92AnueZW?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  under.addEventListener('click', function() {
-      iframeContainer.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6suU8oBlW4O2pg88tOXgHo?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  popButton90.addEventListener('click', function() {
-      popList90.style.display = 'flex';
-      rockList90.style.display = 'none';
-  });
-  
-  wannabe.addEventListener('click', function() {
-      iframeContainer2.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1Je1IMUlBXcx1Fz0WE7oPT?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  britney.addEventListener('click', function() {
-      iframeContainer2.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3MjUtNVVq3C8Fn0MP3zhXa?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  rockButton90.addEventListener('click', function() {
-      rockList90.style.display = 'flex';
-      popList90.style.display = 'none';
-  });
-  
-  dosTierras.addEventListener('click', function() {
-      iframeContainer2.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7BYqVvoXpQFhs4jJ0qqNZt?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  nirvana.addEventListener('click', function() {
-      iframeContainer2.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4CeeEOM32jQcH3eN9Q2dGj?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  wonderwall.addEventListener('click', function() {
-      iframeContainer2.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4yBiZFLXn0n9AUJ5sWO88k?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  popButton00.addEventListener('click', function() {
-      popList00.style.display = 'flex';
-      rockList00.style.display = 'none';
-  });
-  
-  crazy.addEventListener('click', function() {
-      iframeContainer3.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5IVuqXILoxVWvWEPm82Jxr?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  badRomance.addEventListener('click', function() {
-      iframeContainer3.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0SiywuOBRcynK0uKGWdCnn?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  girl.addEventListener('click', function() {
-      iframeContainer3.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5bGmuxShUba9maPswDnhCs?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  rockButton00.addEventListener('click', function() {
-      rockList00.style.display = 'flex';
-      popList00.style.display = 'none';
-  });
-  
-  dios.addEventListener('click', function() {
-      iframeContainer3.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5dQVYFtcLA4pvTRiTFRFkC?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  end.addEventListener('click', function() {
-      iframeContainer3.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/60a0Rd6pjrkxjPbaKzXjfq?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  popButton10.addEventListener('click', function() {
-      popList10.style.display = 'flex';
-      rockList10.style.display = 'none';
-  });
-  
-  shake.addEventListener('click', function() {
-      iframeContainer4.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/71iSmEeF0qRVyULABxP75P?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  adele.addEventListener('click', function() {
-      iframeContainer4.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1c8gk2PeTE04A1pIDH9YMk?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  work.addEventListener('click', function() {
-      iframeContainer4.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/72TFWvU3wUYdUuxejTTIzt?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  rockButton10.addEventListener('click', function() {
-      rockList10.style.display = 'flex';
-      popList10.style.display = 'none';
-  });
-  
-  arctic.addEventListener('click', function() {
-      iframeContainer4.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5FVd6KXrgO9B3JPmC8OPst?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-  
-  radioactive.addEventListener('click', function() {
-      iframeContainer4.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/62yJjFtgkhUrXktIoSjgP2?utm_source=generator&theme=0" width="40%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
-  });
-
-  jQuery(document).ready(function($) {
-    var sections = $('#songs section');
-    var currentIndex = 0;
-  
-    sections.eq(currentIndex).addClass('active');
-  
-    function showSection(index) {
-      sections.removeClass('active');
-      sections.eq(index).addClass('active');
-    }
-  
-    $('.control_prev').click(function() {
-      currentIndex--;
-      if (currentIndex < 0) {
-        currentIndex = sections.length - 1;
-      }
-      showSection(currentIndex);
-    });
-  
-    $('.control_next').click(function() {
-      currentIndex++;
-      if (currentIndex >= sections.length) {
-        currentIndex = 0;
-      }
-      showSection(currentIndex);
-    });
-  });
-  
-  var button = document.getElementById("update-button");
-  var button2 = document.getElementById("update-button2");
-  var button3 = document.getElementById("update-button3");
-  var button4 = document.getElementById("update-button4");
-  var button5 = document.getElementById("update-button5");
-  
-  
-  function changeButtonColor() {
-      button.classList.toggle("clicked");
+function changeButtonColor() {
+    button.classList.toggle("clicked");
     button2.classList.remove("clicked");
     button3.classList.remove("clicked");
     button4.classList.remove("clicked");
     button5.classList.remove("clicked");
+}
   
-    }
+function changeButtonColor2() {
+    button2.classList.toggle("clicked");
+    button.classList.remove("clicked");
+    button3.classList.remove("clicked");
+    button4.classList.remove("clicked");
+    button5.classList.remove("clicked");  
+}
   
-    function changeButtonColor2() {
-      button2.classList.toggle("clicked");
-      button.classList.remove("clicked");
-      button3.classList.remove("clicked");
-      button4.classList.remove("clicked");
-      button5.classList.remove("clicked");
-  
-      
-    }
-  
-    function changeButtonColor3() {
-      button3.classList.toggle("clicked");
+function changeButtonColor3() {
+    button3.classList.toggle("clicked");
     button.classList.remove("clicked");
     button2.classList.remove("clicked");
     button4.classList.remove("clicked");
     button5.classList.remove("clicked");
+}
   
-      
-    }
-  
-    function changeButtonColor4() {
-      button4.classList.toggle("clicked");
+function changeButtonColor4() {
+    button4.classList.toggle("clicked");
     button.classList.remove("clicked");
     button2.classList.remove("clicked");
     button3.classList.remove("clicked");
-    button5.classList.remove("clicked");
+    button5.classList.remove("clicked");   
+}
   
-      
-    }
-  
-    function changeButtonColor5() {
-      button5.classList.toggle("clicked");
+function changeButtonColor5() {
+    button5.classList.toggle("clicked");
     button.classList.remove("clicked");
     button2.classList.remove("clicked");
     button3.classList.remove("clicked");
-    button4.classList.remove("clicked");
-  
-      
-    }
+    button4.classList.remove("clicked");  
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+
+ScrollReveal().reveal('.main-title, .section-title', {delay:500, origin: 'left'});
+ScrollReveal().reveal('.sec1 .image', {delay:600, origin: 'bottom'});
+ScrollReveal().reveal('.text', {delay:700, origin: 'right'});
+ScrollReveal().reveal('.info', {delay:600, origin: 'bottom'});
+ScrollReveal().reveal('.sec2 .image', {delay:600, origin: 'top'});
+ScrollReveal().reveal('.genrepop, .genrerock', {delay:500, origin: 'top'});
+
+
+
+
+
